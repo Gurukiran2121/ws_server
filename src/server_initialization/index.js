0f5d9844b92bbbdf641dd20380629cbe37c5e706
Gurukiran SN <gurukiran.sn@netenrich.com>
2025-05-27 07:48:49 +0530

    updated cors for app

--- src/server_initialization/index.js
+++ src/server_initialization/index.js
@@ -3,6 +3,15 @@ const app = express();
 import http from "http";
 const httpServer = http.createServer(app);
 import { Server } from "socket.io";
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: ["http://localhost:5001", "https://chatsyn.netlify.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 const io = new Server(httpServer, {
   cors: {
